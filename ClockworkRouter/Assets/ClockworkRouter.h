7d3f1b6ea29014fc14d2ba521569927a6dc40c32
Michael Skiba <mike.skiba@atelierclockwork.net>
2018-10-05 09:49:42 +0200

    Feature/initial setup (#1)
    * Created Base Project
    
    - Created Demo App
    - Created Framework
    - Created Tests
    - Got rid of main storyboard
    - Added Swiftlint aggregate target

---
+++ ClockworkRouter/Assets/ClockworkRouter.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for ClockworkRouter.
+FOUNDATION_EXPORT double ClockworkRouterVersionNumber;
+
+//! Project version string for ClockworkRouter.
+FOUNDATION_EXPORT const unsigned char ClockworkRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClockworkRouter/PublicHeader.h>
+
+
