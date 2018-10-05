3703f051a9c13a014ceaac8cdb1e9f3b6a823bcc
Michael Skiba <mike.skiba@atelierclockwork.net>
2018-10-05 16:01:50 +0200

    Added branching routing to settings page

--- ClockworkRouterDemo/Settings/SettingsRouter.swift
+++ ClockworkRouterDemo/Settings/SettingsRouter.swift
@@ -1,6 +1,18 @@
 import UIKit
+import ClockworkRouter
 
-class SettingsRouter: UINavigationController {
+class SettingsRouter: UINavigationController, Router {
+    enum Target: Destination {
+        typealias Container = SettingsRouter
+        static var presentFrom: MainRouter.Target? = .settings
+
+        case one
+        case two
+        case three
+
+        case settings
+    }
+
     init() {
         super.init(nibName: nil, bundle: nil)
         tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 0)
@@ -9,5 +21,33 @@ class SettingsRouter: UINavigationController {
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+
+    func navigate(to destination: SettingsRouter.Target, animated: Bool, completion: ((UIResponder) -> Void)?) {
+        switch destination {
+        case .settings:
+            defer { completion?(visibleViewController ?? self) }
+            guard visibleViewController?.isKind(of: SettingsViewController.self) != true else { return }
+            self.popToRootViewController(animated: true)
+        case .one: push(OneViewController.init, animated: animated, completion: completion)
+        case .two: push(TwoViewController.init, animated: animated, completion: completion)
+        case .three: push(ThreeViewController.init, animated: animated, completion: completion)
+        }
+    }
+
+    func push<Target: UIViewController>(_ provider: () -> Target,
+                                        animated: Bool,
+                                        completion: ((UIResponder) -> Void)?) {
+        guard self.visibleViewController?.isKind(of: Target.self) != true else { return }
+        self.popToRootViewController(animated: animated)
+        let newViewController = provider()
+        self.pushViewController(newViewController, animated: animated)
+        if let coordinator = self.transitionCoordinator {
+            coordinator.animate(alongsideTransition: nil) { [unowned self] _ in
+                completion?(self.visibleViewController ?? self)
+            }
+        } else {
+            completion?(visibleViewController ?? self)
+        }
     }
 }
